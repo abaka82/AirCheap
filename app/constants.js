bda12ce9e2c23be1920d644fbff62a618b812fe9
andry-baka <andry.baka@mitrais.com>
2016-11-17 15:12:55 +0800

    Add further refinement to ACTION types

--- app/constants.js
+++ app/constants.js
@@ -3,3 +3,16 @@ export const RECEIVE_AIRPORTS = 'receive airports';
 export const CHOOSE_AIRPORT = 'choose airport';
 export const REQUEST_TICKETS = 'request tickets';
 export const RECEIVE_TICKETS = 'receive tickets';
+
+
+/*
+You may use a dedicated status field in your actions:
+{ type: 'REQUEST_TICKETS' }
+{ type: 'REQUEST_TICKETS', status: 'error', error: 'Oops' }
+{ type: 'REQUEST_TICKETS', status: 'success', response: { ... } }
+
+Or you can define separate types for them:
+{ type: 'REQUEST_TICKETS_REQUEST' }
+{ type: 'REQUEST_TICKETS_FAILURE', error: 'Oops' }
+{ type: 'REQUEST_TICKETS_SUCCESS', response: { ... } }
+*/
