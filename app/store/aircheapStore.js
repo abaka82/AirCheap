8999345d0040aaa1ea2979538667565c76256521
andry-baka <andry.baka@mitrais.com>
2016-11-17 15:48:56 +0800

    Add axios and redux-logger

--- app/store/aircheapStore.js
+++ app/store/aircheapStore.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
-
+import axios from 'axios';
+//import logger from 'redux-logger';
 
 const logger = (store) => (next) => (action) => {
   if(typeof action !== "function"){
@@ -18,6 +19,15 @@ const aircheapStore = createStore(
   reducers,
   enhancers,
   applyMiddleware(logger, thunk)
+  //applyMiddleware(logger(), thunk)
 );
+
+aircheapStore.dispatch({type: "FETCH_USER_START"});
+
+axios.get('http://rest.learncode.academy/api/wstern/users')
+.then((response) => {
+	aircheapStore.dispatch({type: "FETCH_USER_COMPLETE"})
+	//aircheapStore.dispatch({type: "FETCH_USER_COMPLETE", payload: response.data})
+})
 
 export default aircheapStore;
